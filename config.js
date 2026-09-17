fc11d514ea4998e76038fdaff739e24a18933cbc
Luis Walteros <walteros.dev@gmail.com>
2026-09-17 11:19:07 -0500

    Configuracion de informacion privada

--- config.js
+++
@@ -1,6 +0,0 @@
-// WorkTrack - Configuracion de Supabase
-// Edita estos valores con tus credenciales de Supabase
-window.WORKTRACK_CONFIG = {
-  supabaseUrl: 'https://tjqcowyvzqbdabkgkjvl.supabase.co',
-  supabaseKey: 'sb_publishable__dHxRDvYePu0FklAy2CLBQ_9wNaf5t1'
-};
